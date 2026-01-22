714b293fd75f46d3daa289fb91c5934b0620b29a
Nargis diya <nargisdiyand@gmail.com>
2026-01-22 15:56:41 +0600

    Initial message

---
+++ operator/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // String operator
+    // $a = "Hello";
+    // $b = "Word";
+    // $c = $a . " " . $b;
+    // echo $c;
+
+    // Arithmetic Operator
+    echo (10 + 3) * 4;
+
+    echo "<br>";
+
+    // comparison operator
+    $a = 2;
+    $b = 2;
+    if ($a == $b) {
+        echo "This statement is true!";
+    }
+
+    ?>
+</body>
+
+</html>
