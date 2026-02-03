4e3a20fe35daadaf7781ce64ef0be3b16eaec0dc
Nargis diya <nargisdiyand@gmail.com>
2026-02-03 14:11:37 +0600

    initial message

---
+++ Select Query/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form class="searchform" action="search.php" method="post">
+        <label for="search">Search for user:</label>
+        <input type="text" id="search" name="usersearch" placeholder="Search...">
+        <button>Search</button>
+    </form>
+</body>
+
+</html>
