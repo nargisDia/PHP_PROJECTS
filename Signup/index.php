c85d34db3676fe20b3e342db7fa9faa8be699a1b
Nargis diya <nargisdiyand@gmail.com>
2026-02-05 19:36:31 +0600

    initial message

---
+++ Signup/index.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'includes/config_session.inc.php';
+require_once 'includes/signup_view.inc.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h3>Login</h3>
+
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Login</button>
+    </form>
+
+    <h3>Signup</h3>
+
+    <form action="includes/signup.inc.php" method="post">
+        <?php
+        signup_inputs();
+        ?>
+        <button>Signup</button>
+    </form>
+
+    <?php
+
+    check_signup_errors();
+
+    ?>
+
+</body>
+
+</html>
