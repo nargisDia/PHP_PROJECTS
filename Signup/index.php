3dc38e55222f54b1bfc795a17f245e3ef198c0af
Nargis diya <nargisdiyand@gmail.com>
2026-02-06 12:34:29 +0600

    initial message

--- Signup/index.php
+++ Signup/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'includes/config_session.inc.php';
 require_once 'includes/signup_view.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 
@@ -16,14 +17,31 @@ require_once 'includes/signup_view.inc.php';
 
 <body>
 
-    <h3>Login</h3>
+    <h3>
+        <?php
+        output_username();
+        ?>
+    </h3>
 
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <button>Login</button>
-    </form>
 
+    <?php
+    if (!isset($_SESSION["user_id"])) { ?>
+        <h3>Login</h3>
+
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <button>Login</button>
+        </form>
+    <?php
+    }
+
+    ?>
+
+
+    <?php
+    check_login_errors();
+    ?>
     <h3>Signup</h3>
 
     <form action="includes/signup.inc.php" method="post">
@@ -38,6 +56,13 @@ require_once 'includes/signup_view.inc.php';
     check_signup_errors();
 
     ?>
+
+    <h3>Logout</h3>
+
+    <form action="includes/logout.inc.php" method="post">
+
+        <button>Logout</button>
+    </form>
 
 </body>
 
